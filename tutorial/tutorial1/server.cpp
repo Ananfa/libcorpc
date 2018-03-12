af5c3d3369ff973cb400bc7ef4959555e543c373
liu.xianke <xianke.liu@dena.com>
2018-03-12 11:51:34 +0800

    commit tutorial

---
+++ tutorial/tutorial1/server.cpp
@@ -0,0 +1,58 @@
+//
+//  main.cpp
+//  Tutorial1Server
+//
+//  Created by Xianke Liu on 2018/3/9.
+//  Copyright © 2018年 Dena. All rights reserved.
+//
+
+#include "corpc_routine_env.h"
+#include "corpc_server.h"
+
+#include "helloworld.pb.h"
+
+using namespace CoRpc;
+
+class HelloWorldServiceImpl : public HelloWorldService {
+public:
+    HelloWorldServiceImpl() {}
+    virtual void foo(::google::protobuf::RpcController* controller,
+                     const ::FooRequest* request,
+                     ::FooResponse* response,
+                     ::google::protobuf::Closure* done) {
+        printf("HelloWorldServiceImpl::foo is called\n");
+        
+        std::string msg1 = request->msg1();
+        std::string msg2 = request->msg2();
+        
+        response->set_msg(msg1 + " " + msg2);
+    }
+};
+
+int main(int argc, const char * argv[]) {
+    co_start_hook();
+    
+    if(argc<3){
+        printf("Usage:\n"
+               "Tutorial1Server [IP] [PORT]\n");
+        return -1;
+    }
+    
+    std::string ip = argv[1];
+    unsigned short int port = atoi(argv[2]);
+    
+    // 注册服务
+    IO *io = IO::create(1,1);
+    assert(io);
+    
+    io->start();
+    
+    Server *server = new Server(io, false, 0, ip, port);
+    
+    HelloWorldServiceImpl *helloWorldService = new HelloWorldServiceImpl();
+    server->registerService(helloWorldService);
+    
+    server->start();
+    
+    RoutineEnvironment::runEventLoop();
+}
---
+++ tutorial/tutorial2/server.cpp
@@ -0,0 +1,56 @@
+//
+//  main.cpp
+//  Tutorial2Server
+//
+//  Created by Xianke Liu on 2018/3/9.
+//  Copyright © 2018年 Dena. All rights reserved.
+//
+
+#include "corpc_routine_env.h"
+#include "corpc_server.h"
+
+#include "helloworld.pb.h"
+
+using namespace CoRpc;
+
+class HelloWorldServiceImpl : public HelloWorldService {
+public:
+    HelloWorldServiceImpl() {}
+    virtual void foo(::google::protobuf::RpcController* controller,
+                    const ::FooRequest* request,
+                    ::FooResponse* response,
+                    ::google::protobuf::Closure* done) {
+        printf("HelloWorldServiceImpl::foo is called\n");
+        
+        std::string msg1 = request->msg1();
+        std::string msg2 = request->msg2();
+        
+        response->set_msg(msg1 + " " + msg2);
+    }
+};
+
+int main(int argc, const char * argv[]) {
+    co_start_hook();
+    
+    if(argc<3){
+        printf("Usage:\n"
+               "Tutorial2Server [IP] [PORT]\n");
+        return -1;
+    }
+    
+    std::string ip = argv[1];
+    unsigned short int port = atoi(argv[2]);
+    
+    // 注册服务
+    IO *io = IO::create(1,1);
+    assert(io);
+    Server *server = new Server(io, false, 0, ip, port);
+    
+    HelloWorldServiceImpl *helloWorldService = new HelloWorldServiceImpl();
+    server->registerService(helloWorldService);
+    
+    if (io->start() && server->start()) {
+        RoutineEnvironment::runEventLoop();
+    }
+}
+
