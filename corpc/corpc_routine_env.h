4d8a4860f3745a4407b693d129d7352b3875b9a7
liu.xianke <xianke.liu@dena.com>
2018-03-15 18:06:54 +0800

    fix license comment

--- corpc/corpc_routine_env.h
+++ corpc/corpc_routine_env.h
@@ -1,10 +1,18 @@
-//
-//  co_rpc_routine_env.h
-//  rpccli
-//
-//  Created by Xianke Liu on 2017/11/21.
-//  Copyright © 2017年 Dena. All rights reserved.
-//
+/*
+ * Created by Xianke Liu on 2017/11/21.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 
 #ifndef corpc_routine_env_h
 #define corpc_routine_env_h
--- tutorial/tutorial1/server.cpp
+++ tutorial/tutorial1/server.cpp
@@ -1,10 +1,18 @@
-//
-//  main.cpp
-//  Tutorial1Server
-//
-//  Created by Xianke Liu on 2018/3/9.
-//  Copyright © 2018年 Dena. All rights reserved.
-//
+/*
+ * Created by Xianke Liu on 2018/3/9.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 
 #include "corpc_routine_env.h"
 #include "corpc_server.h"
